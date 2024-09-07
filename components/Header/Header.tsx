bfac6cdea68c86081f5336cc2dd8f6946414cbba
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-08 01:46:34 +0300

    router

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
+import "./header.scss";
 export default function Header() {
-  return (
-    <div>
-      Header
-      
+  return <div className="containerHead">
+    <div  className="whiteLine"></div>
+    <div className="logo"></div>
+    <div className="containerLine ">
+      <div className="longLine"></div>
+      <div className="middleLine"></div>
+      <div className="middleLine"></div>
     </div>
-  )
+  </div>;
 }
