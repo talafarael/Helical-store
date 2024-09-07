9b5bfc6b723791f24f698c38efc8733c89cad786
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-07 15:58:44 +0300

    router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,23 @@
 // import fetchGet from "@/src/api/getApiProduct";
-import { db } from "../src/utils/firebase";
-export default function main({ newData }) {
+
+import { db } from "../utils/firebase";
+interface INewData {}
+export default function main({ newData }: any) {
   console.log(newData);
   return <div>main</div>;
 }
 
 import { collection, getDocs } from "firebase/firestore";
-export const getStaticProps  = async () => {
- const newData=await getDocs(collection(db, "id")).then((querySnapshot) => {
+export const getStaticProps = async () => {
+  const newData = await getDocs(collection(db, "id")).then((querySnapshot) => {
     const newData = querySnapshot.docs.map((doc) => ({
       ...doc.data(),
       id: doc.id,
     }));
-    console.log(newData)
-    return newData
+    console.log(newData);
+    return newData;
   });
   return {
-      props: { newData },
-    };
+    props: { newData },
+  };
 };
--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,5 +1,9 @@
+
 import { useRouter } from "next/router";
 export default function main() {
   const router = useRouter();
-  return <div>{router.query.slug}</div>;
+  return (
+          <div>{router.query.slug}</div>
+  
+  );
 }
