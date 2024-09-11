6020fcc44b83528ae380bc6b8eefd9f8454e7523
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-11 23:48:28 +0300

    font_family

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 import "../app/globals.scss";
 
 import Header from "./Header/header";
@@ -12,15 +12,19 @@ import Header from "./Header/header";
 //   variable: "--font-geist-sans",
 //   weight: "100 900",
 // });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
+const geistMono = localFont({
+  src: "../app/fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+const geistSemiBold= localFont({
+  src: "../app/fonts/IBM_Plex_Sans/IBMPlexSans-Bold.ttf",
+  variable: "--font-semi-bold",
+  weight: "100 900",
+});
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="conatiner">
+    <div className={`${geistSemiBold.variable} ${geistMono.variable}`}>
       <Header />
       {children}
     </div>
