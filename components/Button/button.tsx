f96f09233fb1216b242e9635a4b4c36c2f7f9234
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-15 18:11:01 +0300

    buy

--- components/Button/button.tsx
+++ components/Button/button.tsx
@@ -1,9 +1,33 @@
-import React from "react";
-import "./button.scss"
+import React, { useContext } from "react";
+import "./button.scss";
+import { CountContext } from "@/utils/hooks/context";
+import { useRouter } from "next/router";
 interface IButton {
   text: string;
   func: () => void;
 }
 export default function Button(data: IButton) {
-  return <button className="button">{data.text}</button>;
+  const count = useContext(CountContext);
+  const router = useRouter();
+  const { id } = router.query;
+  const handlerOrder = () => {
+    const localOrder: string | null = localStorage.getItem("order");
+    const order = localOrder ? JSON.parse(localOrder) : [];
+    const existingIndex = order.findIndex(
+      (elem: { id: string }) => elem.id === id
+    );
+
+    if (existingIndex !== -1) {
+      order[existingIndex].count += count?.count 
+    } else {
+      order.push({ count: count?.count, id: id });
+    }
+
+    localStorage.setItem("order", JSON.stringify(order));
+  };
+  return (
+    <button className="button" onClick={handlerOrder}>
+      {data.text}
+    </button>
+  );
 }
--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import logo from "../../public/5400135709693698152-removebg-preview.png";
+import shop from "../../public/shopping-cart.png"
 import "./header.scss";
 import Image from "next/image";
 export default function Header() {
   return (
     <div className="containerHead">
-      <div className="whiteLine"></div>
       <div className="logo">
         <Image className="img" src={logo} alt="" />
       </div>
-      <div className="containerLine "></div>
+      <Image className="shopBin" src={shop} alt="" />
     </div>
   );
 }
