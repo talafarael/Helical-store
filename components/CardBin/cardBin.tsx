4f5f6b2b834cb364ca8dbdb0e76864c0af62125b
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-20 00:59:37 +0300

    input

--- components/CardBin/cardBin.tsx
+++ components/CardBin/cardBin.tsx
@@ -5,7 +5,7 @@ import "./cardBin.scss";
 import Image from "next/image";
 import CountProduct from "../CountProduct/countProduct";
 import { OderContext } from "@/utils/hooks/context";
-import { render } from "sass";
+
 
 export default function CardBin({ data }: { data: IDefaultData }) {
   const order = useContext(OderContext);
@@ -14,20 +14,36 @@ export default function CardBin({ data }: { data: IDefaultData }) {
       console.log(data);
 
       const updatedOrder = order.order.map((element) => {
-        if (data === element.id) {
+        if (data == element.id) {
           return {
             ...element,
-            count: element.count + 1, // Увеличиваем значение корректно
+            count: element.count ++,
           };
         }
         return element;
       });
 
-      order.setOrder(updatedOrder); // Передаём новый массив
-      localStorage.setItem("order", JSON.stringify(updatedOrder)); // Сохраняем новый массив в localStorage
+      order.setOrder(updatedOrder); 
+      localStorage.setItem("order", JSON.stringify(updatedOrder));
     }
   };
-  const handlerMinus = () => {};
+  const handlerMinus = (data: string | undefined) => {};
+  // if (data && order?.order && order.order.count > 1) {
+  //   console.log(data);
+
+  //   const updatedOrder = order.order.map((element) => {
+  //     if (data == element.id) {
+  //       return {
+  //         ...element,
+  //         count: element.count + 1,
+  //       };
+  //     }
+  //     return element;
+  //   });
+
+  //   order.setOrder(updatedOrder); 
+  //   localStorage.setItem("order", JSON.stringify(updatedOrder)); 
+  // }
 
   return (
     <div className="conatinerCardBin">
