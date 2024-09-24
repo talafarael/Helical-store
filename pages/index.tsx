3ecaaf42b3cd58dac785a43469224b593295ce82
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-24 11:20:44 +0300

    return to sstatic pprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import "./index.scss";
 import Card from "@/components/Card/card";
 import { db } from "../utils/firebase";
 import { collection, getDocs } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 interface INewData {
   id: string;
@@ -13,30 +13,29 @@ interface INewData {
   price: string;
   img: string[];
 }
- const Main = () => {
-  const [newData, setNewData] = useState<INewData[] | undefined>();
-
-  useEffect(() => {
-    async function fetchNewData() {
-      const querySnapshot = await getDocs(collection(db, "id"));
-      const data = querySnapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      })) as INewData[];
-      setNewData(data);
-    }
-
-    fetchNewData();
-  }, []);
+const Main = ({ data }: { data: INewData[] }) => {
   return (
     <div className="containerMain">
       <h1 className="titlePage">Home</h1>
       <div className="containerCard">
-        {newData?.map((element) => (
+        {data.map((element) => (
           <Card key={element.id} data={element} />
         ))}
       </div>
     </div>
   );
 };
-export default Main
+export default Main;
+export const getStaticProps = async () => {
+  const querySnapshot = await getDocs(collection(db, "id"));
+  const data = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  })) as INewData[];
+
+  return {
+    props: { data },
+
+    revalidate: 30,
+  };
+};
