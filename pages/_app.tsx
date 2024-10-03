0619103acf247970eb20f4c979edc68e69237272
artem <127351345+talafarael@users.noreply.github.com>
2024-10-03 10:48:10 +0300

    allow any type for fix deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,8 @@ import Layout from "@/components/layout";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-declare global {
-  interface Window {
-    gtag: (command: string, targetId: string, config?: { [key: string]: any }) => void;
-  }
-}
-export {};
+// declare let gtag: Function;
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
@@ -18,9 +14,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const isNoMenuPage = pathsWithoutMenu.some((path) =>
     typeof path === "string" ? path === router.asPath : path.test(router.asPath)
   );
+  
   useEffect(() => {
     const handleRouteChange = (url:string) => {
-      window.gtag('config',  process.env.NEXT_PUBLIC_GA_ID  as string, {
+    (window as any).gtag('config',  process.env.NEXT_PUBLIC_GA_ID  as string, {
         'page_title': document.title,
         'page_location': window.location.href,
         'page_path':  url
