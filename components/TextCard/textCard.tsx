5b643f55d9a3dd8464c01ee36c5bf65acdcbcd43
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-05 18:28:42 +0300

    enlarge the wrapper

--- components/TextCard/textCard.tsx
+++ components/TextCard/textCard.tsx
@@ -1,8 +1,7 @@
-
 import React from "react";
 import "./textCard.scss";
 
-import hryvnia from "@/public/ukraine-hryvnia-icon.svg"
+import hryvnia from "@/public/ukraine-hryvnia-icon.svg";
 import { INewData } from "@/type/newData";
 
 import Deliver from "../Deliver/deliver";
@@ -12,31 +11,26 @@ import Image from "next/image";
 
 export default function TextCard({ newData }: { newData: INewData | null }) {
   return (
-    <div>
+ <>
       {newData && (
         <div className="textContainer">
           <h1 className="nameCard">{newData.name}</h1>
           <h4></h4>
-           <h4 className="index">{newData?.index}</h4>
+          <h4 className="index">{newData?.index}</h4>
           <div className="buttonContainer">
-           
-            <h1 className="priceCard">{newData.price}
-       <Image src={hryvnia} 
-              width={20}
-              height={20} alt="грн">
-
-              </Image>
+            <h1 className="priceCard">
+              {newData.price}
+              <Image src={hryvnia} width={20} height={20} alt="грн"></Image>
             </h1>
-           
+
             <ButtonContainer />
           </div>
-         
           <h6 className="description">опис</h6>
           <p className="descDescription"> {newData?.description}</p>
           <Deliver deliver={newData.deliver} />
           <div className="line"></div> <Description newData={newData} />
         </div>
       )}
-    </div>
+</>
   );
 }
