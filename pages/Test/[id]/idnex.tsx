57f321e7ca610d80bd5e45f1aee8503924cdd4b9
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-09 00:24:58 +0300

    ma

--- pages/Test/[id]/idnex.tsx
+++ pages/Test/[id]/idnex.tsx
@@ -1,6 +1,7 @@
 // "use client";
 import { useParams } from "next/navigation";
-export const generateStaticParams()
+
+
 export default function Post() {
   const params = useParams(); // Fails on client
   return <h1>Slug is {params?.slug}</h1>;
