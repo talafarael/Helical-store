74e423f52a02eb151bc1abc1a556b01a033c5d75
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-08 23:43:58 +0300

    Update index.tsx

--- pages/Test/[id]/index.tsx
+++ pages/Test/[id]/index.tsx
@@ -1,13 +1,28 @@
-export default async function Main() {
-  return <div>text</div>;
+export default function Main({ id }: { id: string }) {
+  return <div>Post ID: {id}</div>;
 }
+
 export const getStaticPaths = () => {
   return {
     paths: [
-      // if no `locale` is provided only the defaultLocale will be generated
       { params: { id: "post-1" }, locale: "en-US" },
       { params: { id: "post-1" }, locale: "fr" },
     ],
-    fallback: true,
+    fallback: true, // or 'blocking' or false based on your needs
+  };
+};
+
+export const getStaticProps = async ({
+  params,
+}: {
+  params: { id: string };
+}) => {
+  // Fetch data based on params.id or do something dynamic
+  const { id } = params;
+
+  return {
+    props: {
+      id, // pass the id as a prop to the page
+    },
   };
 };
