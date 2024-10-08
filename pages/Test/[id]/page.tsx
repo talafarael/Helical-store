f406a04e7c753525049cf4b491c481bba232e100
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-09 00:23:53 +0300

    apge

--- pages/Test/[id]/page.tsx
+++ pages/Test/[id]/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+// "use client";
 import { useParams } from "next/navigation";
-
+export const generateStaticParams()
 export default function Post() {
   const params = useParams(); // Fails on client
   return <h1>Slug is {params?.slug}</h1>;
