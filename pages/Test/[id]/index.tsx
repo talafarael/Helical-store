233d868d5876b4d5959e1473fcbb05af76a92d82
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-08 23:40:46 +0300

    test

--- pages/Test/[id]/index.tsx
+++ pages/Test/[id]/index.tsx
@@ -1,3 +1,13 @@
 export default async function Main() {
-    return <div>text</div>
+  return <div>text</div>;
 }
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      // if no `locale` is provided only the defaultLocale will be generated
+      { params: { id: "post-1" }, locale: "en-US" },
+      { params: { id: "post-1" }, locale: "fr" },
+    ],
+    fallback: true,
+  };
+};
