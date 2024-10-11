cb9e285ccf55bc80eece9f4be9670679ad6443bf
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-11 19:04:37 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,9 @@ import Layout from "@/components/layout";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { motion } from "framer-motion";
-import { AnimatePresence } from "framer-motion";
 
-export default function MyApp({ Component, pageProps, router }: AppProps) {
+
+export default function MyApp({ Component, pageProps}: AppProps) {
   return (
     <Layout>
       <Head>
@@ -18,18 +17,9 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
           href="/signal-wifi-svgrepo-comWhiteTwo.svg"
         />{" "}
       </Head>
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={router.route}
-          className="motion"
-          initial={{ opacity: 0 }} 
-          animate={{ opacity: 1 }} 
-          exit={{ opacity: 0 }} 
-          transition={{ duration: 0.5 }}
-        >
+      
           <Component {...pageProps} />{" "}
-        </motion.div>
-      </AnimatePresence>
+        
     </Layout>
   );
 }
