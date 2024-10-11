a261b808593b312b767da0e94193d8412256a92b
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-11 18:12:22 +0300

    change animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
-const variants = {
-  initial: { opacity: 0, x: -100 },
-  animate: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: 100 },
-};
+
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
@@ -26,11 +22,10 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
         <motion.div
           key={router.route}
           className="motion"
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          variants={variants}
-          transition={{ duration: 0.3 }}
+          initial={{ opacity: 0 }} 
+          animate={{ opacity: 1 }} 
+          exit={{ opacity: 0 }} 
+          transition={{ duration: 0.5 }}
         >
           <Component {...pageProps} />{" "}
         </motion.div>
