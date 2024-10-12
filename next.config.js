2cbb40eba3af1d09ada2edb35269075debe38ddc
talafarael <127351345+talafarael@users.noreply.github.com>
2024-10-12 12:57:06 +0300

    next

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   images: {
     unoptimized: true,
   },
- 
+  trailingSlash: true,
  reactStrictMode: true,
     env: {
         "TOKEN_TELEGRAM":"8036647450:AAG5QB1xCZmpaVTE-2cSIufAVB6T7RpheCE",
